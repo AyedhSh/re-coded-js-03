5c690afdbb7850ae0fb51b19ab58f3f889295260
Gabe Jackson <gj@mail.co.de>
2017-03-27 11:44:33 +0300

    add starter code

--- js/linter.js
+++ js/linter.js
@@ -1,1 +1,23 @@
-// Code your solution here.
+window.onload = () => addLinterListener();
+
+function addLinterListener() {
+  const lintButton = document.getElementById('lint');
+  lintButton.addEventListener('click', lintCode)
+}
+
+// No need to edit anything above this line.
+
+function lintCode() {
+  // Code your solution here:
+  
+}
+
+function getContentFromTextArea() {
+  // Code your solution here:
+  
+}
+
+function addResultToPage(result) {
+  // Code your solution here:
+  
+}
